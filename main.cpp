99ae9e18da9c0f861f64cb44093e2337f3b6f5df
kylewong001 <147275869+kylewong001@users.noreply.github.com>
2023-10-25 00:44:52 -0700

    Add files via upload
    Basic string manipulation. Searching for target characters in strings, and filtering for characters.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string input;
+int count = 0;
+cout<<"Please enter a string: ";
+getline(cin, input);
+
+for(int i = 0; i <input.size(); i++){
+    if (input[i] == ' '){
+        count++;
+    }
+}
+cout << "The total amount of spaces in your string is " << count << endl;
+
+string str;
+cout << "\nPlease enter a string: ";
+getline(cin, str);
+for(int i = 0; i<str.size(); i++){
+    if(isdigit(str[i])){
+        cout<<str[i];
+    }
+}
+
+return 0;
+}
